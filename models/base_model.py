e66842b90093f9660bfe27bdc860a9ce84974dab
MoeAlAnnan <5653@holbertonstudents.com>
2023-11-01 19:22:45 +0000

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,49 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
+""" Module for BaseModel """
 import uuid
 from datetime import datetime
+import models
+import models.engine.file_storage
 
 
-class BaseModel:
-    """A base class for all hbnb models"""
+"""Create BaseModel"""
+
+
+class BaseModel():
+    """ defines all common attributes/methods for other classes """
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
+        if kwargs:
+            self.updated_at = datetime.strptime(kwargs.get('updated_at'),
+                                                '%Y-%m-%dT%H:%M:%S.%f')
+            self.name = kwargs.get('name')
+            self.my_number = kwargs.get('my_number')
+            self.id = kwargs.get('id')
+            self.created_at = datetime.strptime(kwargs.get('created_at'),
+                                                '%Y-%m-%dT%H:%M:%S.%f')
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            models.storage.new(self)
 
     def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        """String Doc"""
+        print_str = "[{}] ({}) {}"
+        return print_str.format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        from models import storage
+        """ updates the current datetime and saves it"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        """
+        Returns a Dictionary with __dict__ method, adds
+        a __class__ attribute value, converts created_at and
+        updated_at datetimes
+        """
+        dic = (self.__dict__).copy()
+        dic['__class__'] = type(self).__name__
+        dic['created_at'] = self.created_at.isoformat()
+        dic['updated_at'] = self.updated_at.isoformat()
+        return dic
