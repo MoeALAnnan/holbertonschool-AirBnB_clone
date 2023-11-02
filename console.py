9d491c8402da37c531e9d8f2b7169fe482ca23ed
MoeAlAnnan <5653@holbertonstudents.com>
2023-11-02 17:24:13 +0000

    console

---
+++ console.py
@@ -0,0 +1,22 @@
+import cmd
+"""create console"""
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program"""
+        print("")
+        return True
+
+    def emptyline(self):
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
