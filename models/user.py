c56b6d2c4f07fbbaac1fde6239ca2dff5f847b4b
MoeAlAnnan <5653@holbertonstudents.com>
2023-11-02 18:55:53 +0000

    user.py

--- models/user.py
+++ models/user.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
-"""A class representing a User"""
+"""This module defines a class User"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """A class representing a User"""
-
-    def __init__(self, *args, **kwargs):
-        """Initialize a User instance"""
-        super().__init__(*args, **kwargs)
-        self.email = kwargs.get('email', '')
-        self.password = kwargs.get('password', '')
-        self.first_name = kwargs.get('first_name', '')
-        self.last_name = kwargs.get('last_name', '')
-
-    def __str__(self):
-        """Return a string representation of the User instance"""
-        return "[User] ({}) {}".format(self.id, self.__dict__)
+    """This class defines a user by various attributes"""
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
